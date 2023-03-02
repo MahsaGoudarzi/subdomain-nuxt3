0d10c507abc65012f6c178faea1b4e6bd8b0f6e1
Mahsa Goodarzi <mhsagodarzii@gmail.com>
2023-03-02 10:51:55 +0330

    added some comments

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,30 +1,43 @@
 import type { RouterOptions } from "@nuxt/schema";
-import { createMemoryHistory } from "vue-router";
 export default <RouterOptions>{
   routes: (_routes) => {
+    //_routes give us all the routes
     const currentUrl = useCookie("currentUrl").value;
-    let url = null;
+    let slug = null;
     let page = "";
-    if (currentUrl)
-      url = currentUrl.match(/(?<=\b(localhost:3000[/]|yourdomain.com[/])\b).+/g);
-    if (url) {
-      page = url[0];
-      if (currentUrl.charAt(currentUrl.length - 1) === "/")
-        page = page.substring(0, url.length - 1);
+    if (currentUrl) {
+      //extract slug
+      slug = currentUrl.match(/(?<=\b(localhost:3000[/]|yourdomain.com[/])\b).+/g);
+    }
+    if (slug) {
+      page = slug[0];
+      if (currentUrl.charAt(currentUrl.length - 1) === "/"){
+        //delete last backslash from page name if there's any
+        page = page.substring(0, slug.length - 1);
+      }
     }
 
     const { ssrContext } = useNuxtApp();
     let subdomain = useCookie("subdomain").value;
+    console.log('subdomain',subdomain)
     if (ssrContext?.event.context.subdomain) {
       subdomain = ssrContext?.event.context.subdomain;
       useCookie("subdomain").value = subdomain;
     }
     if (subdomain) {
+      // here we check if the subdomain exist filter the routes and find 
+      // the index.vue page of the folder with same name as subdomain 
+      // also if there is any slug find the page in the folder with same name as subdomain
+
       const userRoute = _routes.filter((i) => {
         if (page) return i.path == `/${subdomain}/${page}`;
         else return i.path == `/${subdomain}`;
       });
 
+
+      // based on what we filtered in previous section we replace the path with proper value
+      // if theres' any page(slug) in line 48 to 50 , 4 example : subdomain1/about will replace with /about
+      // if there's no page line 51 to 53, 4 example : subdomain1/ will replace with / in your url
       const userRouteMapped = userRoute.map((i) => ({
         ...i,
         path: page
