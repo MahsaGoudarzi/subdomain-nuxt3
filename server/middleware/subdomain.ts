23a43ce1f07237d8fad8b3c37e36cd34714269b1
Mahsa Goodarzi <mhsagodarzii@gmail.com>
2023-09-24 14:29:18 +0330

    delete use event.req and use header

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -4,12 +4,11 @@ export default defineEventHandler((event) => {
   let subdomain = getCookie(event, "subdomain") || null;
   const headers = getHeaders(event)
   const hostname = headers.host || "yourhost.com"
-  
   if (!mainDomain.includes(hostname)) {
     const currentHost = hostname.match(/^[^.]*/g)[0];
     event.context.subdomain = currentHost;
     setCookie(event, "subdomain", currentHost);
-    if(event.req.headers.referer)
-      setCookie(event, "currentUrl", event.req.headers.referer);
+    if(headers.referer)
+      setCookie(event, "currentUrl", headers.referer);
   } 
 }) 
