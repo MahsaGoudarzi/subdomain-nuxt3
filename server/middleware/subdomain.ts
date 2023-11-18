11aa108758831982205b8e35791958f4617c09f6
Mahsa Goodarzi <mhsagodarzii@gmail.com>
2023-11-19 00:15:01 +0330

    update middleware

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,13 +1,18 @@
 import { mainDomain } from "@/constData/app";
 import { getCookie, getHeaders } from "h3";
+
 export default defineEventHandler((event) => {
-  const headers = getHeaders(event)
-  const hostname = headers.host || "yourhost.com"
+  const headers = getHeaders(event);
+  const hostname = headers.host ?? "yourhost.com";
+
   if (!mainDomain.includes(hostname)) {
     const currentHost = hostname.match(/^[^.]*/g)[0];
     event.context.subdomain = currentHost;
+
     setCookie(event, "subdomain", currentHost);
-    if(headers.referer)
+
+    if (headers.referer) {
       setCookie(event, "currentUrl", headers.referer);
-  } 
-}) 
+    }
+  }
+});
